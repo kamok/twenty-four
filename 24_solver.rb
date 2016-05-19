a46e4d61751990aa851f8fde11c752f3c456dccf
Ka Mok <heykamok@gmail.com>
2016-05-19 18:42:21 -0400

    Refactor to follow OOP standards

--- 24_solver.rb
+++ 24_solver.rb
@@ -1,37 +1,29 @@
 require 'pry'
 class TwentyFour
-
-  # def self.solve(a)
-  #   set_of_operations = ["+","-","/","*"].repeated_permutation(3).to_a #[["+", "+", "+"], ["+", "+", "-"]...]
-  #   current_value = 0
-
-  #   operator = set_of_operations[0][0].to_sym 
-  #   # current_value = current_value operator 1
-
-  #   # set_of_operations.each_with_index do |operations, index|
-  #   #   current_value= a[0] operations[index] a[1]
-  #   #   binding.pry
-  #   #   p current_value
-  #   # end
-  
-  # #(1 + 2) + 3) * 4) = 24
-  # end 
-
   def self.solve(a)
+    get_set_of_operations                           #[["+", "+", "+"], ["+", "+", "-"]...]
+    get_unique_set_of_numbers(a)                    #[[1, 2, 3, 4], [1, 2, 4, 3]...]
+  end 
+  
+  private
+
+  def self.get_set_of_operations
     set_of_operations = ["+","-","/","*"].repeated_permutation(3).to_a
-    set_of_numbers = a.repeated_permutation(4).to_a
-    good_set = []
+  end
+
+  def self.get_unique_set_of_numbers(a)
+    set_of_numbers = a.repeated_permutation(4).to_a #[[1, 1, 1, 1], [1, 1, 1, 2]...] not unique
+
+    unique_set = []
 
     set_of_numbers.each do |permutation|
       if permutation.uniq == permutation
-        good_set << permutation
+        unique_set << permutation
       end
     end
 
-    good_set
+    unique_set
   end
 end
-
-
 
 p TwentyFour.solve([1,2,3,4])
