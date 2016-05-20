373bd8455e12a043c1a06ea4966d8487a47a6aed
Ka Mok <heykamok@gmail.com>
2016-05-19 22:10:02 -0400

    Add logic to write out all possible number order and operations, but running into problem with order of operations.

--- 24_solver.rb
+++ 24_solver.rb
@@ -1,10 +1,14 @@
 require 'pry'
+
 class TwentyFour
+
   def self.solve(a)
-    get_set_of_operations                           #[["+", "+", "+"], ["+", "+", "-"]...]
-    get_unique_set_of_numbers(a)                    #[[1, 2, 3, 4], [1, 2, 4, 3]...]
+    operations = get_set_of_operations                 #64 sets [["+", "+", "+"], ["+", "+", "-"]...]
+    numbers = get_unique_set_of_numbers(a)             #24 sets [[1, 2, 3, 4], [1, 2, 4, 3]...]  
+    make_possible_solutions(numbers, operations)
+    find_solutions
   end 
-  
+
   private
 
   def self.get_set_of_operations
@@ -13,17 +17,40 @@ class TwentyFour
 
   def self.get_unique_set_of_numbers(a)
     set_of_numbers = a.repeated_permutation(4).to_a #[[1, 1, 1, 1], [1, 1, 1, 2]...] not unique
-
-    unique_set = []
+    unique_sets = []
 
     set_of_numbers.each do |permutation|
       if permutation.uniq == permutation
-        unique_set << permutation
+        unique_sets << permutation
       end
     end
 
-    unique_set
+    unique_sets.each do|set|
+      set.map! { |num| num.to_f }
+    end
+    unique_sets 
+  end
+
+  def self.make_possible_solutions(nums, ops)
+    @possible_solutions = []
+
+    nums.each do |num|
+      ops.each do |op|
+        @possible_solutions << num.zip(op).flatten.join
+      end 
+    end
+    @possible_solutions
+  end
+
+  def self.find_solutions
+    solutions = []
+    @possible_solutions.each do |equation|
+      if eval(equation) == 24
+        solutions << equation
+      end
+    end
+    solutions
   end
 end
 
-p TwentyFour.solve([1,2,3,4])
+p TwentyFour.solve([2,12,5,3])
